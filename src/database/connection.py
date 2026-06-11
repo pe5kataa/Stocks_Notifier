19b6045e9c72d84b60eb996dfef275c614c3d0a2
pe5kataa <petar.nikodimov@gmail.com>
2026-06-12 00:12:08 +0200

    create a connection to DB

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,3 +1,13 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("")
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
