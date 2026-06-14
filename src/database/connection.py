ddcea1a0fd9036a1071137454aae2217b26256d3
pe5kataa <petar.nikodimov@gmail.com>
2026-06-14 23:56:45 +0200

    update connection to azure db

--- src/database/connection.py
+++ src/database/connection.py
@@ -10,4 +10,6 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+engine = create_engine(
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
+)
